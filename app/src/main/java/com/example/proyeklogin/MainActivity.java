27126d6cbfe1c5503c24e6a220f538317314db70
JMP-AdiMY <k0bas3n@gmail.com>
2021-08-16 07:49:02 +0700

    commit pertama

---
+++ app/src/main/java/com/example/proyeklogin/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.proyeklogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editUsername, editPassword, editEmail, editNamaLengkap, editAsalSekolah, editAlamatTinggal;
+    Button btnSimpan;
+    TextView textViewPassword;
+
+    public static final String FILENAME = "login";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Halaman Depan");
+
+        editUsername = findViewById(R.id.editUsername);
+        textViewPassword = findViewById(R.id.textViewPassword);
+        editPassword = findViewById(R.id.editPassword);
+        editEmail = findViewById(R.id.editEmail);
+        editNamaLengkap = findViewById(R.id.editNamaLengkap);
+        editAsalSekolah = findViewById(R.id.editAsalSekolah);
+        editAlamatTinggal = findViewById(R.id.editAlamatTinggal);
+        btnSimpan = findViewById(R.id.btnSimpan);
+
+        btnSimpan.setVisibility(View.GONE);
+        editUsername.setEnabled(false);
+        editPassword.setVisibility(View.GONE);
+        textViewPassword.setVisibility(View.GONE);
+        editEmail.setEnabled(false);
+        editNamaLengkap.setEnabled(false);
+        editAsalSekolah.setEnabled(false);
+        editAlamatTinggal.setEnabled(false);
+
+        bacaFileLogin();
+    }
+
+    void bacaFileLogin() {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard,FILENAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br =
+                        new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Error " + e.getMessage());
+            }
+            String data = text.toString();
+            String[] dataUser = data.split(";");
+            bacaDataUser(dataUser[0]);
+
+        }
+    }
+
+    void bacaDataUser(String fileName) {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard,FILENAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br =
+                        new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Error " + e.getMessage());
+            }
+            String data = text.toString();
+            String[] datauser = data.split(";");
+
+            editUsername.setText(datauser[0]);
+            editEmail.setText(datauser[2]);
+            editNamaLengkap.setText(datauser[3]);
+            editAsalSekolah.setText(datauser[4]);
+            editAlamatTinggal.setText(datauser[5]);
+
+        } else {
+            Toast.makeText(this, "User Tidak Ditemukan", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutambah, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                tampilkanDialogKonfirmasiLogout();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    void tampilkanDialogKonfirmasiLogout() {
+        new AlertDialog.Builder(this)
+                .setTitle("Logout")
+                .setMessage("Apakah Anda Yakin Ingin Logout?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichbutton) {
+                        hapusFile();
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null).show();
+    }
+}
